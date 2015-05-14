fa1d2d35a957d6eb9e09b538a58de8d94b27bae2
Dennis Timmermann <timmermann.dennis@googlemail.com>
2015-05-14 22:43:53 +0200

    already a new build system -platform io- and first working gps code

--- arduino/src/lua.ino
+++ arduino/src/lua.ino
@@ -10,18 +10,37 @@
 // Pin 6  has the LED on Teensy++ 2.0
 // Pin 13 has the LED on Teensy 3.0
 // give it a name:
+
+#include "TinyGPS++.h"
 int led = 13;
+
+TinyGPSPlus gps;
 
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize the digital pin as an output.
   pinMode(led, OUTPUT);
+
+  Serial.begin(9600);
+  Serial1.begin(9600);
+
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(200);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(200);               // wait for a second
+	if(Serial1.available() > 0) {
+		digitalWrite(led, HIGH);
+		while(Serial1.available() > 0) {
+			gps.encode(Serial1.read());
+		}
+		//Serial.println(gps.failedChecksum());
+		Serial.print(gps.location.lat());
+		Serial.print(" - ");
+		Serial.println(gps.location.lng());
+	}
+	digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+
+	//Serial.print(" - ");
+	//Serial.println(gps.satellites.value());
+	//delay(1000);
 }
